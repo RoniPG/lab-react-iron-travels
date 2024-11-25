a51bd109f1a734fe2cd766ec8634d7f994f6efb6
RoniPG <ronigmn@gmail.com>
2024-11-25 12:54:53 +0100

    Iteration 2 | Conditional Rendering - Labels

--- src/components/travel-list/travel-list.jsx
+++ src/components/travel-list/travel-list.jsx
@@ -15,6 +15,12 @@ function TravelList() {
                         <h3>{tpd.destination} ({tpd.days} Days)</h3>
                         <p>{tpd.description}</p>
                         <p><strong>Price:</strong> {tpd.totalCost} €</p>
+                        <div className="container d-flex flex-row gap-2">
+                        {tpd.totalCost <= 350 && <label className="bg-success rounded px-2"><strong>Great Deal</strong></label> }
+                        {tpd.totalCost >= 1500 && <label className="bg-primary text-light rounded px-2"><strong>Premium</strong></label> }
+                        {tpd.allInclusive && <label className="bg-primary text-light rounded px-2"><strong>All-Inclusive</strong></label> }
+                        </div>
+                        
                     </div>
                 </div>
             ))}
